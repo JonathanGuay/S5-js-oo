ee25233a7a2484a397752d09b0c360b0d00c28b4
1424439 <1424439@cegepoutaouais.qc.ca>
2023-02-08 15:52:10 -0500

    je suis perdu

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ class fruit{
         this.poire = poire;
         this.pomme = pomme;
     }
+    ToString(){
+        return "Total de pommes : " + this.pomme + "\nTotal de pêches : " + this.peche +
+            "\nTotal de poires : " + this.poire;
+    }
     AjouterTotal(){
         let prixTotal = x.nouveauPricePeche + x.nouveauPricePoire + x.nouveauPricePomme;
         //document.querySelector("#totale").textContent = +prixTotal + " $";
@@ -29,6 +33,10 @@ class Panier{
         this.qte = qte;
         this.prix = prix;
         this.prixtotal = prixtotal;
+    }
+    ToString(){
+        return "La quantité est : " + this.qte + "Le prix est : " + this.prix +
+            "Le prix total : " + this.prixtotal;
     }
     Afficher(fruit){
         if (fruit === "pomme"){
@@ -46,13 +54,13 @@ class Panier{
     }
     UpdateQte(fruit){
         if (fruit === "pomme"){
-            x.QtePomme = x.QtePomme + this.qte;
+            x.QtePomme = this.qte;
         }
         else if(fruit === "peche"){
-            x.QtePeche = x.QtePeche + this.qte;
+            x.QtePeche = this.qte;
         }
         else{
-            x.QtePoire = x.QtePoire + this.qte;
+            x.QtePoire =  this.qte;
         }
     }
     UpdatePrix(){
@@ -67,61 +75,88 @@ const Total = new fruit(x.nouveauPricePeche, x.nouveauPricePoire, x.nouveauPrice
 function ajouterPomme(){
     // let nouveauPricePomme = 0;
     let QtePomme = document.getElementById("nbPommes").value;
+    x.QtePomme = QtePomme;
     let prixPomme = 11;
     //let prixPomme = document.getElementById("prixPommes").value;
     x.nouveauPricePomme = prixPomme * QtePomme;
-    const Pomme = new Panier(QtePomme, prixPomme, x.nouveauPricePomme);
+    const Pomme = new Panier(x.QtePomme, prixPomme, x.nouveauPricePomme);
     Pomme.Afficher("pomme");
-    Pomme.UpdateQte("pomme")
     Pomme.UpdatePrix();
-    Pomme.UpdateQteTotal();
     Total.AjouterTotal();
+    //Pomme.UpdateQte("pomme")
+    Pomme.UpdateQteTotal();
 }
 
 function ajouterPoire(){
     // let nouveauPricePoire = 0;
     let QtePoire = document.getElementById("nbPoires").value;
+    x.QtePoire = QtePoire;
     let prixPoire = 12;
     //let prixPoire = document.getElementById("prixPoires").value;
     x.nouveauPricePoire = prixPoire * QtePoire;
-    const Poire = new Panier(QtePoire, prixPoire, x.nouveauPricePoire);
+    const Poire = new Panier(x.QtePoire, prixPoire, x.nouveauPricePoire);
     Poire.Afficher("poire");
-    Poire.UpdateQte("poire")
     Poire.UpdatePrix();
-    Poire.UpdateQteTotal();
     Total.AjouterTotal();
+    //Poire.UpdateQte("poire")
+    Poire.UpdateQteTotal();
 }
 
 function ajouterPeche(){
     // let nouveauPricePeche = 0;
     let QtePeche= document.getElementById("nbPeches").value;
+    x.QtePeche = QtePeche;
     let prixPeche = 10;
     //let prixPeche = document.getElementById("prixPeches").value;
     x.nouveauPricePeche = prixPeche * QtePeche;
-    const Peche = new Panier(QtePeche, prixPeche, x.nouveauPricePeche);
+    const Peche = new Panier(x.QtePeche, prixPeche, x.nouveauPricePeche);
     Peche.Afficher("peche");
-    Peche.UpdateQte("peche")
     Peche.UpdatePrix();
-    Peche.UpdateQteTotal();
     Total.AjouterTotal();
+    //Peche.UpdateQte("peche")
+    Peche.UpdateQteTotal();
 }
 
 // fonction qui fonctionne pas
 function valider(){
-    //let prixTotal = document.getElementById("totale")[0].value;
-    //let prixTotal = document.querySelector("#totale").value;
-    //let prixTotal = document.forms[0]["totale"].value;
+    // quand la valeur à aller chercher n'est pas dans un input, ne pas mettre value mais innerText
+    //let prixTotal = document.getElementById("totale")[0].innerText;
+    //let prixTotal = document.querySelector("#totale").innerText;
+    //let prixTotal = document.forms[0]["totale"].innerText;
     // const qteTotal = new Panier(x.QtePeche, x.QtePoire, x.QtePomme);
-    // let qteTotal = x.QtePeche + x.QtePoire + x.QtePomme;
-    if (x.PrixTotal >= 20 && x.QteTotal <= 25){
+    let QtePeche = document.getElementById("nbPeches").value;
+    let QtePoire = document.getElementById("nbPoires").value;
+    let QtePomme = document.getElementById("nbPommes").value;
+    let qteTotal = QtePeche + QtePoire + QtePomme;  // ca fonctionne pas
+    if (x.PrixTotal >= 20 && qteTotal <= 25) {
         document.getElementById("erreur").setAttribute("class", "invisible");
+        document.getElementById("erreur2").setAttribute("class", "invisible");
         return true;
     }
     else{
-        document.getElementById("erreur").setAttribute("class", "error");
+        if (x.PrixTotal <= 20){
+            document.getElementById("erreur").setAttribute("class", "error");
+        }
+        else {
+            document.getElementById("erreur").setAttribute("class", "invisible");
+        }
+        if (qteTotal >= 25){
+            document.getElementById("erreur2").setAttribute("class", "error");
+        }
+        else{
+            document.getElementById("erreur2").setAttribute("class", "invisible");
+        }
         return false;
     }
 }
+function afficher(){
+    document.writeln(Total.ToString());
+}
+
+// pour inscrire les données dans la session
+// sessionStorage.setItem("nbPomme", document.getElementByID("nbPommes").value);
+
+
 
 // fonction pas utile à cause le readonly fait toute la job
 /*function able(){
@@ -138,3 +173,37 @@ function valider(){
         return false;
     }
 }*/
+
+/*
+// récupéré les données dans l'url quand on utilise la méthod get dans le form
+let data = location.search.substring(1); // trouve la location des données dans l'URL
+document.write(data);
+let tabDatas = data.split('&');
+let nbPeches = tabDatas[0].split('=')[1];
+document.write(nbPeches)
+
+// récupérer les données de la session
+let nbPommes = sessionStorage.getItem("nbPommes");
+document.write(nbPommes);
+
+document.writeln(); // cela sert à afficher des choses
+
+let panier = sessionStorage.getItem("panier")
+document.writeln(panier);
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
