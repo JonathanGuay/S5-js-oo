832546ef41e576782fd2451a8b324eb54c4fb775
1424439 <1424439@cegepoutaouais.qc.ca>
2023-02-10 13:40:36 -0500

    je suis perdu encore plus

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ class fruit{
     pomme = 0;
     poire = 0;
     peche = 0;
-    constructor(peche, poire, pomme){
+    constructor(peche = 0, poire = 0, pomme = 0){
         this.peche = peche;
         this.poire = poire;
         this.pomme = pomme;
@@ -22,14 +22,14 @@ class fruit{
             "\nTotal de poires : " + this.poire;
     }
     AjouterTotal(){
-        let prixTotal = x.nouveauPricePeche + x.nouveauPricePoire + x.nouveauPricePomme;
+        x.prixTotal = x.nouveauPricePeche + x.nouveauPricePoire + x.nouveauPricePomme;
         //document.querySelector("#totale").textContent = +prixTotal + " $";
-        document.querySelector("#totale").textContent = +prixTotal;
+        document.querySelector("#totale").textContent = +x.prixTotal;
     }
 }
 class Panier{
     prixtotal = 0;
-    constructor(qte, prix, prixtotal){
+    constructor(qte = 0, prix = 0, prixtotal = 0){
         this.qte = qte;
         this.prix = prix;
         this.prixtotal = prixtotal;
@@ -64,13 +64,13 @@ class Panier{
         }
     }
     UpdatePrix(){
-        x.PrixTotal = x.PrixTotal + this.prix;
+         x.PrixTotal = x.PrixTotal + this.prix;
     }
     UpdateQteTotal(){
-        x.QteTotal = x.QteTotal + this.qte;
+         x.QteTotal = x.QteTotal + this.qte;
     }
 }
-const Total = new fruit(x.nouveauPricePeche, x.nouveauPricePoire, x.nouveauPricePomme);
+Total = new fruit(x.nouveauPricePeche, x.nouveauPricePoire, x.nouveauPricePomme);
 
 function ajouterPomme(){
     // let nouveauPricePomme = 0;
@@ -79,7 +79,7 @@ function ajouterPomme(){
     let prixPomme = 11;
     //let prixPomme = document.getElementById("prixPommes").value;
     x.nouveauPricePomme = prixPomme * QtePomme;
-    const Pomme = new Panier(x.QtePomme, prixPomme, x.nouveauPricePomme);
+     Pomme = new Panier(x.QtePomme, prixPomme, x.nouveauPricePomme);
     Pomme.Afficher("pomme");
     Pomme.UpdatePrix();
     Total.AjouterTotal();
@@ -94,7 +94,7 @@ function ajouterPoire(){
     let prixPoire = 12;
     //let prixPoire = document.getElementById("prixPoires").value;
     x.nouveauPricePoire = prixPoire * QtePoire;
-    const Poire = new Panier(x.QtePoire, prixPoire, x.nouveauPricePoire);
+    Poire = new Panier(x.QtePoire, prixPoire, x.nouveauPricePoire);  // regarder si le const fuck le program
     Poire.Afficher("poire");
     Poire.UpdatePrix();
     Total.AjouterTotal();
@@ -109,7 +109,7 @@ function ajouterPeche(){
     let prixPeche = 10;
     //let prixPeche = document.getElementById("prixPeches").value;
     x.nouveauPricePeche = prixPeche * QtePeche;
-    const Peche = new Panier(x.QtePeche, prixPeche, x.nouveauPricePeche);
+     Peche = new Panier(x.QtePeche, prixPeche, x.nouveauPricePeche);
     Peche.Afficher("peche");
     Peche.UpdatePrix();
     Total.AjouterTotal();
@@ -124,11 +124,11 @@ function valider(){
     //let prixTotal = document.querySelector("#totale").innerText;
     //let prixTotal = document.forms[0]["totale"].innerText;
     // const qteTotal = new Panier(x.QtePeche, x.QtePoire, x.QtePomme);
-    let QtePeche = document.getElementById("nbPeches").value;
+    /*let QtePeche = document.getElementById("nbPeches").value;
     let QtePoire = document.getElementById("nbPoires").value;
     let QtePomme = document.getElementById("nbPommes").value;
-    let qteTotal = QtePeche + QtePoire + QtePomme;  // ca fonctionne pas
-    if (x.PrixTotal >= 20 && qteTotal <= 25) {
+    let qteTotal = QtePeche + QtePoire + QtePomme;  // ca fonctionne pas*/
+    if (x.PrixTotal >= 20 && x.QteTotal <= 25) {
         document.getElementById("erreur").setAttribute("class", "invisible");
         document.getElementById("erreur2").setAttribute("class", "invisible");
         return true;
@@ -140,7 +140,7 @@ function valider(){
         else {
             document.getElementById("erreur").setAttribute("class", "invisible");
         }
-        if (qteTotal >= 25){
+        if (x.QteTotal >= 25){
             document.getElementById("erreur2").setAttribute("class", "error");
         }
         else{
@@ -151,12 +151,9 @@ function valider(){
 }
 function afficher(){
     document.writeln(Total.ToString());
+    // pour inscrire les données dans la session
+   // sessionStorage.setItem("nbPomme", document.getElementByID("nbPommes").value);
 }
-
-// pour inscrire les données dans la session
-// sessionStorage.setItem("nbPomme", document.getElementByID("nbPommes").value);
-
-
 
 // fonction pas utile à cause le readonly fait toute la job
 /*function able(){
@@ -165,11 +162,11 @@ function afficher(){
     //let submit = document.getElementById("submit").value;
 
     if (element === true){
-        document.getElementById("submit").setAttribute("readonly", "");
+        document.getElementById("submit").setAttribute("disabled", "true");
         return true;
     }
     else {
-        document.getElementById("submit").setAttribute("readonly", "readonly");
+        document.getElementById("submit").setAttribute("disabled", "readonly");
         return false;
     }
 }*/
